0f7d1e2eb25de1c2000609fa4f34a6a35b45b68d
Bojan Nikolic <bojan@bnikolic.co.cuk>
2013-12-01 12:35:48 +0000

    Begin factoring out the rotational constants

--- examples/hcxnspecmodel.R
+++ examples/hcxnspecmodel.R
@@ -2,7 +2,18 @@ library('devtools');
 library('ggplot2');
 load_all("/home/bnikolic/d/n/RNested/")
 
-
+nuX <- function(X)
+  {
+    xx=read.csv("hcxconsts.csv", header=T, row.names=1 )
+    ccb <- xx[X, "ccb"]
+    ccd <- xx[X, "ccd"]
+    nu <- function(J)
+      {
+        JUp <- J+1;
+        2*ccb*JUp - 4*ccd*JUp**3;
+      }
+    nu
+  }
 
 hcxnS <- function(p, J)
   {
@@ -58,6 +69,20 @@ mkLkl <- function()
     nested.GaussLkl(dd$J, dd$S, function(x,p) {hcxnS(p,x) }, dd$SErr);
   }
 
+prepp <- function()
+  {
+    pp <- c( -12, -9, 0, 1)
+    lpf <- boxp(pp)
+    ss <- sset.box(array( pp , dim=c(2,length(pp)/2)),
+                   100, 
+                   mkLkl())
+    list(lpf=lpf, ss=ss)
+  }
+
 
 # optim( c(-9,0.5) , function(p) { -1*xx(p)}) check convergance with.
 # ggplot(dd) + geom_errorbar(aes(L, S, ymin=S-SErr, ymax=S+SErr, colour='red')) + geom_point(aes(L, hcxnS(c(-9.333890,  0.659134), dd$L)))
+
+# ggplot(dd) + geom_errorbar(aes(L, S, ymin=S-SErr, ymax=S+SErr, colour='red')) + geom_point(aes(L, hcxnS(c(-9.333890,  0.659134), dd$L))) + facet_grid(. ~ L, scales="free_x") + theme(axis.text.x=element_blank() ,axis.ticks.x = element_blank() ,panel.grid.minor.x=element_blank(), panel.grid.major.x=element_blank())
+
+#ggplot(dd) + scale_y_continuous(limits=c(0, 1.25))+ geom_errorbar(aes(L, S, ymin=S-SErr, ymax=S+SErr, colour='red')) + geom_point(aes(L, hcxnS(c(-9.333890,  0.659134), dd$L))) + facet_grid(. ~ L, scales="free_x") +  theme_bw()+ theme(axis.text.x=element_blank() ,axis.ticks.x = element_blank() ,panel.grid.minor=element_blank(), panel.grid.major=element_blank(), axis.title.x = element_text(face="bold", size=12),          axis.title.y = element_text(face="bold", size=12, angle=90), legend.title=element_blank())+     stat_abline(intercept=0, slope=0, linetype="dotted")
